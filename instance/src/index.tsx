d3adfd8bd98773357b7cde0a9e5addd22c21be23
Stephane de Wit <stephane.dewit@gaming1.com>
2021-11-30 17:47:06 +0100

    Upgrade Piral and install Blazor plugin

--- instance/src/index.tsx
+++ instance/src/index.tsx
@@ -1,6 +1,7 @@
 import 'piral/polyfills';
 import { renderInstance } from 'piral';
 import { layout, errors } from './layout';
+import { createBlazorApi } from 'piral-blazor';
 
 // change to your feed URL here (either using feed.piral.cloud or your own service)
 const feedUrl = 'https://feed.piral.cloud/api/v1/pilet/empty';
@@ -8,6 +9,7 @@ const feedUrl = 'https://feed.piral.cloud/api/v1/pilet/empty';
 renderInstance({
   layout,
   errors,
+  plugins: [createBlazorApi()],
   requestPilets() {
     return fetch(feedUrl)
       .then(res => res.json())
